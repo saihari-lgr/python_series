5f972b881d43b1741c0658141c00aef36c89d72b
saihari-lgr <97013205+saihari-lgr@users.noreply.github.com>
2024-08-24 16:11:11 +0530

    Create 02_Numbers.py
    Demonstrating Numbers

---
+++ Fundamentals/02_Numbers.py
@@ -0,0 +1,18 @@
+
+print('Basic Arthematic operators')
+
+print('Addition: \t',2+1)
+print('Substraction: \t',2-1)
+print('Multiplication: \t',2*2)
+print('Division: \t',7/2)
+print('Floor Division: \t', 7//2)  # floor division truncates decimals and return the integer values
+
+print('Modulo: \t',  7%2) # 2 goes into 7 thrice, with a remainder of 1. % operator returns the remainder after division
+
+print('Power of: \t', 2**3)
+print('root of: \t', 9**0.5)
+print('root of: \t', 10**0.5)
+
+
+
+
